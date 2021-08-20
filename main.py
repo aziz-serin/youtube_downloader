42aeb0601a67d2ff4e611af8f1ec66bc1d265e9d
d95703as <aziz.serin@student.manchester.ac.uk>
2021-08-20 22:54:55 +0300

    made a small function template, will continue later.

---
+++ main.py
@@ -0,0 +1,16 @@
+from pytube import YouTube
+
+def display_info(media):
+    return
+
+def fetch_media(link):
+    return
+
+def terminal_interface():
+    return
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
